d1eb9e1f65254bbcc5b850c5c3196f0ca75506e5
patpatpanda <nils-emil1337@hotmail.se>
2024-11-08 16:39:42 +0100

    Commit message

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -27,7 +27,7 @@ export default function Hero() {
         loop
         className={styles.backgroundVideo}
       >
-        <source src="/images/dust.mp4" type="video/mp4" />
+        <source src="/images/set.mp4" type="video/mp4" />
         Din webbläsare stöder inte videouppspelning.
       </video>
 
